5e9abda48c65bdb3fe7492d4ab059fabbe1b98f5
Yariyari <yaritza.urbina.sanhueza@gmail.com>
2022-09-12 11:12:01 -0300

    cachipun

---
+++ ejemplo_cachipun.py
@@ -0,0 +1,11 @@
+opciones = ["piedra", "tijera", "papel"]
+
+print("Hola,juguemos cachipun")
+
+print ("Ingresa su opción")
+print (1, "piedra")
+print (2, "tijera")
+print (3, "papel")
+
+mano1 = int(input("jugador1\n"))
+mano1_eleccion = opciones[mano1 - 1]
---
+++ lista.py
@@ -0,0 +1,23 @@
+
+students = [
+  'Abel',
+  'Aylin',
+  'Charlotte',
+  'Claudia',
+  'Elsy',
+  'Javiera',
+  'Karen',
+  'Maria soledad',
+  'Maryelin',
+  'Nicolas',
+  'Paola',
+  'Paola M.',
+  'Silvia',
+  'Yaritza'
+]
+user_option = input ('Selecciona tu posicion\n')
+
+index = int(user_option) -1
+student = students[index]
+
+print(student)
